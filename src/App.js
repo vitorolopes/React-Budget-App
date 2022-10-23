8b54d7e300c676e1a3ce19d2a218d9d1f2813f67
vitor lopes <lopesvitor959@gmail.com>
2022-10-23 18:12:54 +0100

    Basic HTML/CSS.<BudgetCard>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Stack, Button } from "react-bootstrap";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add a Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      <div style={{
+         display: "grid",
+         gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+         gap: "1rem",
+         alignItems: "flex-start"
+      }}>
+        <BudgetCard
+          name="Entertainment"
+          amount={600}
+          max={1000}
+          gray
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        </BudgetCard>
+
+      </div>
+     
+    </Container>
   );
 }
 
