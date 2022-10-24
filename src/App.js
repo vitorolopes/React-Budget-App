b2c1d610d74baf96a15e47a87e4172d2ef9419b5
vitor lopes <lopesvitor959@gmail.com>
2022-10-24 15:51:46 +0100

    <AddBudgetModal> HTML

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Container, Stack, Button } from "react-bootstrap";
+import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
 import { useBudgetsContext } from "./context/BudgetsContextProvider";
 
@@ -9,31 +10,35 @@ function App() {
   console.log(dummyValue);
 
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add a Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add a Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
 
-      <div style={{
-         display: "grid",
-         gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-         gap: "1rem",
-         alignItems: "flex-start"
-      }}>
-        <BudgetCard
-          name="Entertainment"
-          amount={600}
-          max={1000}
-          gray
-        >
+        <div style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start"
+        }}>
+          <BudgetCard
+            name="Entertainment"
+            amount={600}
+            max={1000}
+            gray
+          >
 
-        </BudgetCard>
+          </BudgetCard>
 
-      </div>
-     
-    </Container>
+        </div>
+      
+      </Container>
+
+      <AddBudgetModal show={true} handleClose={() => console.log("test close")}/>
+    </>
   );
 }
 
